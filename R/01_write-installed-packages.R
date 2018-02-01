dea64ba74b8a362a97e59a9664a4ac8c7b35fe78
John David Smith <john.smith@learningalliances.net>
2018-02-01 11:38:04 -0800

    step 1. get data, subset, write a .csv to the ./data directory

--- R/01_write-installed-packages.R
+++ R/01_write-installed-packages.R
@@ -1,6 +1,16 @@
+#' ---
+#' output: github_document
+#' ---
+
 ## deja vu from yesterday!
+library(tidyverse)
+library(fs)
+library(here)
 
 ## create a data frame of your installed packages
+ipt <- installed.packages() %>%
+  as_tibble() %>%
+  select(Package, LibPath, Version, Priority, Built)
 
 ## keep the variables
 ##   * Package
@@ -10,6 +20,9 @@
 ##   * Built
 
 ## write it to data/installed-packages.csv
+
+write_csv(ipt, here("data", "installed-packages.csv"))
+
 ## YES overwrite the file that is there now
 ## that came from me (Jenny)
 ## it an example of what yours should look like
